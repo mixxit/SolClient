63f9088c1ccd0dd48c1ade79e837f88123c8fefb
Mike Bolland <mike.bolland@ajw-aviation.com>
2015-05-23 13:02:29 +0100

    t4

---
+++ SolClient/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+using System.Collections;
+
+public class BoardManager : MonoBehaviour {
+
+	[Serializable]
+	public class Count
+	{
+		public int minimum;
+		public int maximum;
+
+		public Count (int min, int max)
+		{
+			minimum = min;
+			maximum = max;
+		}
+
+	}
+
+	public int columns = 8;
+	public int rows = 8;
+	public Count wallCount = new Count(5, 9);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
